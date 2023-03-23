cae0d0d331ca39ec278c9110f0ab6f7e800bbcb9
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2023-03-23 11:36:03 +0000

    Integrated Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
@@ -22,3 +23,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.config('2e9b1042165e4bb38f5d5a728feff9da').install();
